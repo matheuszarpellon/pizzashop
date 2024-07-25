e5a36977af91ad0ddcdfcaa112eac6df0be4b0aa
Matheus Zarpellon <mvzarpellon@gmail.com>
2024-07-25 02:58:15 -0300

    Estrutura inicial da aplicação e pagina de login

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Router } from "@remix-run/router";
+
+export const router: Router = createBrowserRouter([
+
+  { path: "/", element: <AppLayout />, children: [{ path: "/", element: <Dashboard /> }] },
+  { path: "/", element: <AuthLayout />, children: [{ path: "/sign-in", element: <SignIn /> }] },
+  { path: "/sign-in", element: <SignIn /> }
+])

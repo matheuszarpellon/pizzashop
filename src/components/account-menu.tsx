3255b28d4f5fe38b7bc328c5cf5d150c02a16365
Matheus Zarpellon <mvzarpellon@gmail.com>
2024-07-31 22:04:38 -0300

    logout

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,14 +1,18 @@
-import { DropdownMenu, DropdownMenuLabel, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuSeparator, DropdownMenuItem } from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
-import { Building, ChevronDown, LogOut } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
-import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
-import { Skeleton } from "./ui/skeleton";
-import { Dialog, DialogTrigger } from "./ui/dialog";
+import { getProfile } from "@/api/get-profile";
+import { signOut } from "@/api/sign-out";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Building, ChevronDown, LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { Button } from "./ui/button";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -19,6 +23,13 @@ export function AccountMenu() {
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigninOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    }
   })
 
   return (
@@ -51,9 +62,11 @@ export function AccountMenu() {
               <span>Perfil da Loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="h-4 w-4 mr-2" />
-            <span>Sair</span>
+          <DropdownMenuItem asChild className="text-rose-500 dark:text-rose-400" disabled={isSigninOut}>
+            <button className="w-full" onClick={() => signOutFn}>
+              <LogOut className="h-4 w-4 mr-2" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
